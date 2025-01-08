671af80eb9afad495d2f2faeaa34995fa9478ff0
unknown <tifuhvirginie@yahoo.com>
2025-01-07 22:28:09 -0500

    yes

--- index.js
+++ index.js
@@ -31,8 +31,12 @@ const authenticateToken = (req, res, next) => {
 };
 
 // Middleware
-app.use(cors());
-// app.use(express.json());  
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+  allowedHeaders: ['Content-Type', 'Authorization'],  
+})); 
 
 // Routes
 app.use('/api/auth', authRoutes);  
